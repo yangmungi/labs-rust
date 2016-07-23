b722f64ff1d36ae65bfd62e782760bebf106753b
Yangmun Choi <yangmungi@ucla.edu>
2016-07-23 00:57:23 -0700

    Adding most of functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
+extern crate rand;
+
+use std::io;
+use std::env::args;
+use std::i32;
+
+use rand::Rng;
 
 fn main() {
+    let args = &mut args();
+    let input_number = &args.nth(1)
+        .expect("must have exactly 1 arg");
 
+    let number = match i32::from_str_radix(input_number, 10) {
+        Ok(output) => output,
+        Err(e) => panic!(e)
+    };
+
+    let thread_rng = &mut rand::thread_rng();
+
+    for _ in 0..number {
+        let rn = thread_rng.gen_range(1, 10);
+        print!("{}", rn);
+    }
 }
