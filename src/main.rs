d026bdcfae0b3320afa362451c8015c4b8059f39
Yangmun Choi <yangmungi@ucla.edu>
2016-07-23 21:27:15 -0700

    Adding more things, mostly brainstorming.

--- src/main.rs
+++ src/main.rs
@@ -48,15 +48,30 @@ fn logical_handle<KeyType: Eq + Hash, ValueType: Eq>(mut old_values: HashMap<Key
     }
 }
 
-fn stream_handle<StreamType: Read>(mut stream: StreamType) {
+fn stream_handle<StreamType: Read>(mut stream: StreamType) -> ChangeRequest<String, Option<()>> {
     // transfer protocol
-    // u8 coded length for key
-    // first 2 bits
-    //   00 -> next 6 bits = u8 -> 0 - 64 bytes
-    //   01 -> num is 6 + 8 bits = u16 -> 0 - 16384 bytes
-    // the next two seem impractical...
-    //   10 -> num is 6 + 24 bits = u32 -> 0 - 2^30 bytes
-    //   11 -> num is 6 + 56 bits = u64 -> 0 - 2^62 bytes
+    // ----
+    // u8-1 coded length 
+    // first bit
+    //   0 -> next 7 bits = u8 -> 0 - 2^7 = 128 bytes
+    //   1 -> num is 7 + 8 bits = u16 -> 0 - 2^15 = 32768 bytes
+
+    // u8-2 coded length 
+    // first 2 bits used to determine read length
+    //   00 -> next 6 bits = 0 - 2^6 = 64 bytes 
+    //   01 -> 6 + 8 = 0 - 2^14 = 16384 bytes
+    //   10 -> 6 + 24 = 0 - 2^30 = 1,073,741,824 ~ 1G bytes
+    //   11 -> 6 + 56 = 0 - 2^62 = 4.612 x 10^18 ~ 4E bytes
+    // overall, u8-2 schema seems excessive for data, we don't have 4E block devices
+
+    // maybe u8
+    // read_len = u8 _+_ u8
+    // next read_len bytes for key
+    //  key-class = read_len
+    // u8-1 coded length for old
+    // old bytes
+    // u8-1 coded length for new
+    // new bytes
 
     //let mut buffer = [0; 10];
     //try!(stream.read(&mut buffer[..]));
@@ -68,6 +83,8 @@ fn stream_handle<StreamType: Read>(mut stream: StreamType) {
     }
 
     println!("Read: \"{}\"", buffer);
+
+    ChangeRequest { key: String::from("test-key"), old: None, new: None }
 }
 
 fn main() {
@@ -88,7 +105,7 @@ fn main() {
 
                     println!("Connected {}", peer_addr);
 
-                    stream_handle(&mut stream);
+                    let change_request = stream_handle(&mut stream);
 
                     stream.shutdown(Shutdown::Both)
                         .expect("Could not shut down");
